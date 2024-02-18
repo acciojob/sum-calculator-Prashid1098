e03945acc5ad4eecc09d01e446b5fd34a7e30fb8
Prashid Pal <67044025+Prashid1098@users.noreply.github.com>
2024-02-18 17:28:37 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,22 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [number,setNum]=useState(0);
+
+    function updateNum(event)
+    {
+      let n=parseInt(event.target.value);
+      setNum(prevNum => prevNum + n);
+    }
+
   return (
     <div>
         {/* Do not remove the main div */}
+         <div>Sum Calculator:</div><br/>
+        <input type="number" id="abc" onChange={updateNum} />
+        <p id="abc">Sum: {number}</p>
     </div>
   )
-}
+}  
 
 export default App
